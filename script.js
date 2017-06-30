6642f5586e9de798c52b4d834af7baae7ea156b5
srini vasan <sriniss13@gmail.com>
2017-06-30 18:16:28 +0530

    sri

---
+++ script.js
@@ -0,0 +1,59 @@
+
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider,$locationProvider) {
+    $routeProvider
+    .when("/home", {
+    	templateUrl :"home/home.html"
+    })
+    .when("/project", {
+        templateUrl : "project/project.html"
+    })
+
+    .when("/qualification", {
+    	templateUrl : "qualification/qualification.html"
+    })
+    .when("/contactus",{
+    	templateUrl : "contactus/contactus.html"
+    })
+    .when("/contactme",{
+    	templateUrl :"contactme/contactme.html"
+    	
+    })
+    .when("/hobbies",{
+    	templateUrl : "hobbies/hobbies.hxtml"
+    })
+    .when("/certification",{
+    	templateUrl : "certification/certification.html"
+        
+    })
+    .when("/Resume",{
+    	templateUrl : "/index.html"
+    })
+    .otherwise({"redirectTo":'/home'})
+    $locationProvider.html5Mode(true);
+    
+});
+
+app.controller("nav",function($scope, $http){
+
+        $http.get("sample.json").then(function(response){
+            console.log(response);
+           $scope.navbar=response.data.navbar;
+           $scope.dropdown=response.data.dropdown;
+        }); 
+ 				
+
+ 				
+ });
+
+
+    
+ 
+  
+  	
+ 
+
+
+
+
+     
